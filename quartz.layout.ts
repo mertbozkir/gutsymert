fc2e2f4a12a3abfb76e02b1972f34cbe72b4e796
Mert Bozkır <mert.bozkirr@gmail.com>
2023-12-04 09:25:13 +0300

    Prettier pfff

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,7 +16,12 @@ export const sharedPageComponents: SharedLayout = {
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta(), Component.TagList()],
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+    Component.TagList(),
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
@@ -27,7 +32,9 @@ export const defaultContentPageLayout: PageLayout = {
         title: "Recent Thoughts",
         limit: 4,
         filter: (f) =>
-          f.slug!.startsWith("thoughts/") && f.slug! !== "thoughts/index" && !f.frontmatter?.noindex,
+          f.slug!.startsWith("thoughts/") &&
+          f.slug! !== "thoughts/index" &&
+          !f.frontmatter?.noindex,
         linkToMore: "thoughts/" as SimpleSlug,
       }),
     ),
@@ -48,7 +55,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs() ,Component.ArticleTitle()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
